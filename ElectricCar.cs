b49bb2cd1cf4d97579ed75999184d39fa609b8bb
Detective-Coder <aaronfrankenfield@gmail.com>
2021-04-26 09:41:47 -0400

    first commit

---
+++ ElectricCar.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace garys_wholesale_garage
+  {
+  public class Tesla  // Electric car
+  {
+      public double BatteryKWh { get; set; }
+      public string MainColor { get; set; }
+      public string MaximumOccupancy { get; set; }
+
+      public void ChargeBattery()
+      {
+          // method definition omitted
+      }
+  }
+}
