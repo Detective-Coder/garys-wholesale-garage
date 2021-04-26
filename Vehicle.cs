3d5e0b123fd27f38eeea91fcb5e8aadb2564b2ff
Detective-Coder <aaronfrankenfield@gmail.com>
2021-04-26 09:47:09 -0400

    renamed files, added vehicle class

---
+++ Vehicle.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace garys_wholesale_garage
+  {
+  public class Vehicle
+  {
+      public string MainColor { get; set; }
+      public string MaximumOccupancy { get; set; }
+  }
+}
